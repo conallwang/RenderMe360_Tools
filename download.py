b82dc3df5d7dfc1a9c276f018c66af2d5feca1b2
conallwang <conallwang@tencent.com>
2024-06-26 10:05:49 +0800

    update download.py

--- download.py
+++ download.py
@@ -4,13 +4,16 @@
 import openxlab
 from openxlab import dataset
 
-openxlab.login(ak="YOUR ACCESS KEY", sk="YOUR SECRET KEY")  # Login
+YOUR_ACCESS_KEY = "TO BE INSERTED"
+YOUR_SECRET_KEY = "TO BE INSERTED"
+
+openxlab.login(ak=YOUR_ACCESS_KEY, sk=YOUR_SECRET_KEY)  # Login
 dataset.info(dataset_repo="OpenXDLab/RenderMe-360")  # check dataset info
 dataset.query(dataset_repo="OpenXDLab/RenderMe-360")  # check file info in the dataset
 # dataset.get(dataset_repo="OpenXDLab/RenderMe-360", target_path="/path/to/local/folder/")  # download all data
 
 dataset.download(
     dataset_repo="OpenXDLab/RenderMe-360",
-    source_path="/raw/0026",
+    source_path="/raw/0041",
     target_path="/apdcephfs_cq10/share_1467498/cq2/datasets/RenderMe360/",
 )  # Download some files in the dataset
