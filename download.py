d702c5e92378ad3d75aeb7fa55ab1fdea02ded6f
conallwang <conallwang@tencent.com>
2024-06-26 10:00:41 +0800

    add reference website to download.py

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 # Install openxlab using "pip install -U openxlab"
+# Refer to https://opendatalab.com/OpenXDLab/RenderMe-360
 
 import openxlab
 from openxlab import dataset
@@ -6,10 +7,10 @@ from openxlab import dataset
 openxlab.login(ak="YOUR ACCESS KEY", sk="YOUR SECRET KEY")  # Login
 dataset.info(dataset_repo="OpenXDLab/RenderMe-360")  # check dataset info
 dataset.query(dataset_repo="OpenXDLab/RenderMe-360")  # check file info in the dataset
-dataset.get(dataset_repo="OpenXDLab/RenderMe-360", target_path="/path/to/local/folder/")  # download all data
+# dataset.get(dataset_repo="OpenXDLab/RenderMe-360", target_path="/path/to/local/folder/")  # download all data
 
-# dataset.download(
-#     dataset_repo="OpenXDLab/RenderMe-360",
-#     source_path="/raw/0026",
-#     target_path="/apdcephfs_cq10/share_1467498/cq2/datasets/RenderMe360/",
-# )  # Download some files in the dataset
+dataset.download(
+    dataset_repo="OpenXDLab/RenderMe-360",
+    source_path="/raw/0026",
+    target_path="/apdcephfs_cq10/share_1467498/cq2/datasets/RenderMe360/",
+)  # Download some files in the dataset
